31149fbc0c184f34d55f5d3226b4bbb74c8b61e5
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2020-07-22 17:56:24 +0200

    added requirements

--- app.py
+++ app.py
@@ -20,50 +20,66 @@ app.layout = html.Div(
                      html.Div(className='four columns div-user-controls',
                               children=[
                                   html.H2('Stock Market Simulator'),
-                                  html.P('Visualising time series with Plotly - Dash.'),
-                                  html.P('Pick one or more stocks from the dropdown below.'),
+                                  html.P("An artificial market will be populated by two kinds of traders: "
+                                         + "Fundamentalist, that only buy the asset if it is above their valuation "
+                                         + "and sell it if it is below and Chartists that buy or sell according to momentum. "
+                                         + "All traders start with the same wealth of 10000.0 and may submit 30% of their wealth "
+                                         + "in orders in each period (if their wealth goes to zero they cannot submit orders). "
+                                         + "Fundamentalists submit Limit Orders and Chartists submit Market Orders. "
+                                         + "The Valuations for Fundamentalists and the time windows for the momentum indicator "
+                                         + "for Chartists are drawn from a normal distribution (set mean and variance below)."
+                                         ),
+
+                                  html.P('Configure Fundamentalists:'),
+                                  html.Div(
+                                      [
+                                          dcc.Input(
+                                              id="n_fund", type="number",
+                                              debounce=True, placeholder="Number of Traders",
+                                          ),
+                                          dcc.Input(
+                                              id="f_dist_mean", type="number",
+                                              debounce=True, placeholder="Mean Value",
+                                          ),
+                                          dcc.Input(
+                                              id="f_dist_var", type="number",
+                                              debounce=True, placeholder="Variance of Values",
+                                          ),
+                                      ]
+                                  ),
+                                  html.P('Configure Chartists:'),
+                                  html.Div(
+                                      [
+                                          dcc.Input(
+                                              id="n_chart", type="number",
+                                              debounce=True, placeholder="Number of Traders",
+                                          ),
+                                          dcc.Input(
+                                              id="c_dist_mean", type="number",
+                                              debounce=True, placeholder="Mean Window Length",
+                                          ),
+                                          dcc.Input(
+                                              id="c_dist_var", type="number",
+                                              debounce=True, placeholder="Variance of window",
+                                          ),
+                                      ]
+                                  ),
+                                  html.P('Set number of trading periods:'),
                                   html.Div(
                                       [
                                           dcc.Input(
                                               id="length", type="number",
                                               debounce=True, placeholder="length of simulation",
                                           ),
-                                          dcc.Input(
-                                              id="n_fund", type="number",
-                                              debounce=True, placeholder="Number of Fundamentalists",
-                                          ),
-                                          dcc.Input(
-                                              id="n_chart", type="number",
-                                              debounce=True, placeholder="Number of Chartists",
-                                          ),
-                                          dcc.Input(
-                                              id="f_dist_mean", type="number",
-                                              debounce=True, placeholder="Distribution of fundamentalists",
-                                          ),
-                                          dcc.Input(
-                                              id="f_dist_var", type="number",
-                                              debounce=True, placeholder="Distribution of fundamentalists",
-                                          ),
-
-                                          dcc.Input(
-                                              id="c_dist_mean", type="number",
-                                              debounce=True, placeholder="Distribution of chartists",
-                                          ),
-                                          dcc.Input(
-                                              id="c_dist_var", type="number",
-                                              debounce=True, placeholder="Distribution of chartists",
-                                          ),
-                                          # html.Hr(),
-                                          html.Div(id="number-out"),
                                       ]
                                   ),
-                                  html.P('Run Simulation below!'),
                                   html.Button('Simulate!', id='submit-val', n_clicks=0, style={'color': '675438'}),
                               ]
                               ),
                      html.Div(className='eight columns div-for-charts bg-grey',
                               children=[
-                                  dcc.Graph(id='timeseries', config={'displayModeBar': False}, animate=True)
+                                  dcc.Graph(id='timeseries', config={'displayModeBar': False}, animate=True),
+                                  dcc.Graph(id='bar', config={'displayModeBar': False}, animate=True)
                               ]
                               ),
                  ]
@@ -92,6 +108,8 @@ def number_render(fval, tval, rangeval):
                ]
               )
 def update_graph(val, length, n_fund, n_chart, f_dist_mean, f_dist_var, c_dist_mean, c_dist_var):
+    #global data
+
     print(n_fund)
     sim = Simulation(length,
                      n_fund,
@@ -101,18 +119,21 @@ def update_graph(val, length, n_fund, n_chart, f_dist_mean, f_dist_var, c_dist_m
                      )
 
     sim.start()
-    sim.market_prices
+    data = sim.market_prices
+    update_bar(data)
+
+
 
     layout = go.Layout(
-        title="Historic Prices",
+        title="Simulated Prices",
         plot_bgcolor="#FFF",  # Sets background color to white
         xaxis=dict(
-            title="time",
+            title="Price Evolution",
             linecolor="#BCCCDC",  # Sets color of X-axis line
             showgrid=False  # Removes X-axis grid lines
         ),
         yaxis=dict(
-            title="price",
+            title="Prices",
             linecolor="#BCCCDC",  # Sets color of Y-axis line
             showgrid=False,  # Removes Y-axis grid lines
         )
@@ -125,6 +146,33 @@ def update_graph(val, length, n_fund, n_chart, f_dist_mean, f_dist_var, c_dist_m
 
     return figure
 
+
+#@app.callback(Output('bar', 'figure'),[Input('submit-val', "n_clicks")])
+def update_bar(data):
+
+    layout = go.Layout(
+        title="Profits",
+        plot_bgcolor="#FFF",  # Sets background color to white
+        xaxis=dict(
+            title="Traders",
+            linecolor="#BCCCDC",  # Sets color of X-axis line
+            showgrid=False  # Removes X-axis grid lines
+        ),
+        yaxis=dict(
+            title="PnL",
+            linecolor="#BCCCDC",  # Sets color of Y-axis line
+            showgrid=False,  # Removes Y-axis grid lines
+        )
+    )
+
+    figure = go.Figure(
+        data=go.Scatter(x=[i for i in range(len(data))], y=data),
+        layout=layout
+    )
+
+    return figure
+
+
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=False,dev_tools_ui=False,dev_tools_props_check=False)
+    app.run_server(debug=False, dev_tools_ui=False, dev_tools_props_check=False)
